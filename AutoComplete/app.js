d2380b935e5b97468819fb016f92b6416efa4b6c
NaokiOtsu <n.otsu1029@gmail.com>
2017-02-18 22:27:42 +0900

    modified AutoComplete component

--- AutoComplete/app.js
+++ AutoComplete/app.js
@@ -1,15 +1,33 @@
 (function() {
   'use strict';
   
+  var KEY_CODE = {
+    'up': 38,
+    'down': 40,
+    'enter': 13,
+    'escape': 27
+  };
+  
   function AutoComplete() {
+    var _this = this;
+    
+    this.html = document.documentElement;
     this.input = document.querySelector('.auto-complete');
+    this.suggest = document.querySelector('.suggest-contents');
     
     this.data = null; // autoCompleteするデータ
+    this.current = 0;
+    this.currentScroll = 0;
+    this.suggestHeight = 200;
+    this.liHeight = 28;
     
     this.getAutoCompleteData()
-      .then(this.setAutoCompleteData.bind(this), null);
-    
-    this.bindEvents();
+      .then(function(json) {
+        _this.data = json;
+        _this.bindEvents();
+      }, function() {
+        console.log('error');
+      });
   }
   
   AutoComplete.prototype.getAutoCompleteData = function() {
@@ -21,7 +39,6 @@
     });
     
     xhr.addEventListener('error', function(event) {
-      console.log('error');
       defer.reject('error');
     });
     
@@ -32,12 +49,10 @@
     return defer.promise();
   };
   
-  AutoComplete.prototype.setAutoCompleteData = function(json) {
-    this.data = json;
-  };
-  
   AutoComplete.prototype.bindEvents = function() {
     this.input.addEventListener('keydown', this.onKeyDown.bind(this), false);
+    this.html.addEventListener('click', this.onBlur.bind(this), false);
+    this.suggest.addEventListener('click', this.onClickSuggest.bind(this), false);
   };
   
   AutoComplete.prototype.onKeyDown = function(event) {
@@ -46,12 +61,113 @@
     requestAnimationFrame(this.autoComplete.bind(this, event));
   };
   
+  AutoComplete.prototype.onBlur = function() {
+    this.hideSuggest();
+  };
+  
+  AutoComplete.prototype.onEnter = function() {
+    var target = this.suggest.querySelectorAll('li')[this.current - 1];
+    this.hideSuggest();
+    
+    if (!target) return;
+    
+    var text = target.textContent;
+    this.input.value = text;
+  };
+  
+  AutoComplete.prototype.onClickSuggest = function(event) {
+    var text = event.target.textContent;
+    this.input.value = text;
+    this.hideSuggest();
+  };
+  
+  AutoComplete.prototype.hideSuggest = function(event) {
+    this.suggest.textContent = null;
+    this.current = 0;
+  };
+  
   AutoComplete.prototype.autoComplete = function(event) {
     var text = event.target.value.trim();
     
-    if (!text) return;
+    if (!text) {
+      this.hideSuggest();
+      return;
+    } 
     
-    console.log(text)
+    var suggestList = this.getSuggestList(text);
+    this.renderSuggest(suggestList);
+    
+    if (!suggestList.length) return;
+    
+    // console.log(event.keyCode);
+    switch (event.keyCode) {
+      case KEY_CODE.up:
+        this.selectSuggest(-1);
+        break;
+      case KEY_CODE.down:
+        this.selectSuggest(1);
+        break;
+      case KEY_CODE.enter:
+        this.onEnter();
+        break;
+      case KEY_CODE.escape:
+        this.hideSuggest();
+        break;
+      default:
+        break;
+    }
+  };
+  
+  AutoComplete.prototype.selectSuggest = function(num) {
+    var li_list = this.suggest.querySelectorAll('li');
+    var length = li_list.length;
+    
+    this.current += num;
+    
+    if (this.current <= 0) {
+      this.current = 1;
+    } else if (this.current > length) {
+      this.current = length;
+    }
+    
+    li_list.forEach(function(element) {
+      element.classList.remove('over');
+    });
+    li_list[this.current - 1].classList.add('over');
+    
+    this.renderScroll();
+    // this.suggest.scrollTop = 100;
+    
+  };
+  
+  AutoComplete.prototype.renderScroll = function() {
+    var suggest_height = this.suggest.getBoundingClientRect().height;
+    
+    console.log()
+  };
+  
+  AutoComplete.prototype.getSuggestList = function(text) {
+    var results = [];
+    
+    results = this.data['data'].filter(function(value, index, array) {
+      var regexp = new RegExp(text);
+      return regexp.test(value);
+    });
+    
+    return results;
+  };
+  
+  AutoComplete.prototype.renderSuggest = function(list) {
+    var ul = document.createElement('ul');
+    
+    list.forEach(function(value) {
+      var li = document.createElement('li');
+      li.textContent = value;
+      ul.appendChild(li);
+    });
+    
+    this.suggest.textContent = null;
+    this.suggest.appendChild(ul);
   };
   
   window.addEventListener('DOMContentLoaded', function() {
--- AutoComplete/index.php
+++ AutoComplete/index.php
@@ -5,11 +5,43 @@
   <title>AutoComplete</title>
 </head>
 <style>
-
+* {
+  margin: 0;
+  padding: 0;
+}
+input {
+  display: block;
+  border: 1px solid #ccc;
+  line-height: 20px;
+  box-sizing: border-box;
+}
+body {
+  margin: 50px;
+}
+.suggest-container {
+  position: relative;
+  margin-bottom: 50px;
+}
+.suggest-contents {
+  position: absolute;
+  top: 21px;
+  left: 0;
+  max-width: 180px;
+  border: 1px solid #ccc;
+  background-color: white;
+}
+ul { list-style: none; }
+li { padding: 2px 5px; }
+li.over {
+  background-color: #eee;
+}
 </style>
 <body>
 
-<input type="text" class="auto-complete">
+<div class="suggest-container">
+  <input type="text" class="auto-complete">
+  <div class="suggest-contents"></div>
+</div>
 
 <?php include( $_SERVER['DOCUMENT_ROOT'] . '/footer.php'); ?>
 
