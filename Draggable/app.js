aca15459f02353fb32e0ecfe9a39750fd6c7a191
NaokiOtsu <n.otsu1029@gmail.com>
2017-02-19 17:00:24 +0900

    Added Draggable component

---
+++ Draggable/app.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+  
+  function Draggable() {
+    
+    this.bindEvents();
+  }
+  
+  Draggable.prototype.getHeightAllContents = function() {
+    
+  };
+  
+  window.addEventListener('DOMContentLoaded', function() {
+    new Draggable();
+  });
+})();
---
+++ Draggable/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Draggable</title>
+</head>
+<style>
+.box {
+  width: 100px;
+  height: 100px;
+  background: black;
+}
+</style>
+<body>
+
+<div class="box" draggable="true"></div>
+
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/footer.php'); ?>
+
+<script src="./Draggable/app.js"></script>
+</body>
+</html>
